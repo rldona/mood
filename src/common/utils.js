ebd6a25aa0a6cb23de9fc4787424a6cdd059652c
Raúl López <rldona@gmail.com>
2021-01-24 20:42:05 +0100

    feat: added getCurrentDate

--- src/common/utils.js
+++ src/common/utils.js
@@ -3,3 +3,19 @@ export function compare(a, b) {
   if (a.index > b.index) return 1;
   return 0;
 }
+
+export function getCurrentDate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
+  var yyyy = today.getFullYear();
+
+  today = mm + '/' + dd + '/' + yyyy;
+
+  return {
+    full: today,
+    day: parseInt(dd),
+    month: parseInt(mm),
+    year: parseInt(yyyy)
+  };
+}
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Home = () => {
+import { getCurrentDate } from '../../common/utils';
+
+export default function Home() {
+  const [currentDate, setCurrentDate] = useState({});
+
+  useEffect(() => {
+    setCurrentDate(getCurrentDate());
+  }, []);
+
   return (
     <div className="Home">
       <h1>¿Cómo estás de ánimo hoy?</h1>
+      <p>{currentDate.full}</p>
     </div>
   );
 }
-
-export default Home;
