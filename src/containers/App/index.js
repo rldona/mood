ecd6bed9730be6edabfab836cd299f2fae527041
Raúl López <rldona@gmail.com>
2021-01-26 00:19:28 +0100

    feat: added authentication logic routing

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   BrowserRouter as Router,
@@ -9,6 +9,8 @@ import {
 
 import "../../common/firebase";
 
+import { auth } from '../../common/firebase';
+
 import GuardedRoute from '../GuardedRoute';
 import Login from '../Login';
 import LoginGuard from '../LoginGuard';
@@ -17,18 +19,37 @@ import Calendar from '../Calendar';
 
 import './App.css';
 
-const isAuthenticated = () => {
-  return false;
+const validateFirebaseLogin = (setLogged) => {
+  console.log('validando...');
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      setLogged(true)
+    } else {
+      setLogged(null)
+    }
+  });
+}
+
+const isAuthenticated = (isLogged) => {
+  return isLogged;
 }
 
 export default function App() {
+  const [isLogged, setLogged] = useState(false);
+
+  useEffect(() => {
+    validateFirebaseLogin(setLogged);
+  }, []);
+
+  if (isLogged === false) return null;
+
   return (
     <div>
       <Router>
         <Switch>
-          <LoginGuard path='/login' component={Login} auth={isAuthenticated()} />
-          <GuardedRoute path='/home' component={Home} auth={isAuthenticated()} />
-          <GuardedRoute path='/calendar' component={Calendar} auth={isAuthenticated()} />
+          <LoginGuard path='/login' component={Login} auth={isAuthenticated(isLogged)} />
+          <GuardedRoute path='/home' component={Home} auth={isAuthenticated(isLogged)} />
+          <GuardedRoute path='/calendar' component={Calendar} auth={isAuthenticated(isLogged)} />
           <Redirect from="*" to="/home"/>
         </Switch>
       </Router>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
 
+import { auth } from '../../common/firebase';
+
 import { getCurrentDate } from '../../common/utils';
 
 export default function Home() {
   const [currentDate, setCurrentDate] = useState({});
+
+  const logout = () => {
+    console.log('Logout init...');
+
+    auth.signOut().then(() => {
+      console.log('Logout OK');
+    }).catch((error) => {
+      console.log('Logout KO');
+    });
+  }
 
   useEffect(() => {
     setCurrentDate(getCurrentDate());
@@ -13,6 +25,7 @@ export default function Home() {
     <div className="Home">
       <h1>¿Cómo estás de ánimo hoy?</h1>
       <p>{currentDate.full}</p>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
