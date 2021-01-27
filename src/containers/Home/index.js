7d179258d37bbf7c513e85a2f6ded0e9067b9358
Raúl López <rldona@gmail.com>
2021-01-27 19:09:21 +0100

    feat: added authcontext

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 
 import { auth } from '../../common/firebase';
 
+import { useHistory } from "react-router-dom";
+
 import { getCurrentDate } from '../../common/utils';
 
 export default function Home() {
   const [currentDate, setCurrentDate] = useState({});
+  let history = useHistory();
 
   const logout = () => {
     console.log('Logout init...');
@@ -17,6 +20,14 @@ export default function Home() {
     });
   }
 
+  const moodState = (state) => {
+    console.log(state);
+  }
+
+  const goTo = (route) => {
+    history.push(`/${route}`);
+  }
+
   useEffect(() => {
     setCurrentDate(getCurrentDate());
   }, []);
@@ -25,7 +36,14 @@ export default function Home() {
     <div className="Home">
       <h1>¿Cómo estás de ánimo hoy?</h1>
       <p>{currentDate.full}</p>
-      <button onClick={logout}>Logout</button>
+      <br />
+      <button onClick={() => moodState('happy')}>Feliz</button>
+      <button onClick={() => moodState('neutral')}>Normal</button>
+      <button onClick={() => moodState('bad')}>Mal</button>
+      <br /><br />
+      <button onClick={() => goTo('calendar')}>Ver el calendario</button>
+      <br /><br />
+      <button onClick={logout}>Salir</button>
     </div>
   );
 }
