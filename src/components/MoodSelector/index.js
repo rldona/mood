e8a340f7909af68c83a81ff6ff4f84391983fe4e
Raúl López <rldona@gmail.com>
2021-02-05 23:48:41 +0100

    feat: added icons

--- src/components/MoodSelector/index.js
+++ src/components/MoodSelector/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import ThumbsUpDownIcon from '@material-ui/icons/ThumbsUpDown';
+
 import './MoodSelector.css';
 
 export default function MoodSelector(props) {
@@ -9,9 +13,21 @@ export default function MoodSelector(props) {
 
   function MoodList() {
 
-    const moodItemComponent = props.moodList.map(mood =>
-      <li key={mood.id} mood={mood} onClick={() => onMoodSelected(mood)}>{mood.state} === {mood.isActive ? 'true' : 'false'}</li>
-    );
+    const moodItemComponent = props.moodList.map(mood => {
+      if (mood.state === 'happy') {
+        return <li key={mood.id} mood={mood} onClick={() => onMoodSelected(mood)}>{mood.state} === {mood.isActive ? 'true' : 'false'} <ThumbUpIcon /></li>
+      }
+
+      if (mood.state === 'neutral') {
+        return <li key={mood.id} mood={mood} onClick={() => onMoodSelected(mood)}>{mood.state} === {mood.isActive ? 'true' : 'false'} <ThumbsUpDownIcon /></li>
+      }
+
+      if (mood.state === 'bad') {
+        return <li key={mood.id} mood={mood} onClick={() => onMoodSelected(mood)}>{mood.state} === {mood.isActive ? 'true' : 'false'} <ThumbDownIcon /></li>
+      }
+
+      return true;
+    });
 
     return (
       <ul>{ moodItemComponent }</ul>
